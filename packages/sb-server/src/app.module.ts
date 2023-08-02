5ba69dceeea5c2f2978426fd983c409c68e21d08
Maksym Shapovalov <maks0681912507@gmail.com>
2023-08-01 20:56:26 -0400

    Feat(sb-server): configure DB connection

--- packages/sb-server/src/app.module.ts
+++ packages/sb-server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { DatabaseModule } from 'configs/mysql';
 
 @Module({
-    controllers: [AppController],
+    imports: [DatabaseModule],
 })
 export class AppModule {}
