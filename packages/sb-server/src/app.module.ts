d8322000f6d7a8b65296359425fc705ed3d121bd
Maksym Shapovalov <maks0681912507@gmail.com>
2023-08-02 20:09:31 -0400

    Feat(sb-server): add DTO validation. Fix DB connection config

--- packages/sb-server/src/app.module.ts
+++ packages/sb-server/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'configs/mysql';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { MYSQL_CONFIG } from 'configs/mysql.config';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync(MYSQL_CONFIG),
+    ],
+    exports: [ConfigModule],
 })
 export class AppModule {}
--- packages/sb-server/src/main.ts
+++ packages/sb-server/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { VALIDATION_CONFIG } from 'configs/validation.config';
 
 const PORT = 5000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe(VALIDATION_CONFIG));
     await app.listen(PORT);
 }
+
 bootstrap();
