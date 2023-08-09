c4b8c5919945c8da6aa8b02895c75d1ff34f2d8e
Maksym Shapovalov <maks0681912507@gmail.com>
2023-08-09 00:43:36 -0400

    Fix(sb-server): data validation, add @nestjs/swagger

--- packages/sb-server/src/configs/validation.config.ts
+++ packages/sb-server/src/configs/validation.config.ts
@@ -1,10 +1,6 @@
 import { ValidationPipeOptions } from '@nestjs/common';
-import * as classValidator from '@nestjs/class-validator';
-import * as classTransformer from '@nestjs/class-transformer';
 
 export const VALIDATION_CONFIG: ValidationPipeOptions = {
-    validatorPackage: classValidator,
-    transformerPackage: classTransformer,
     transform: true,
     whitelist: true,
     forbidNonWhitelisted: true,
