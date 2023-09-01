109b76793a748a200e3d60dfac74e9459d561ae1
Maksym Shapovalov <maks0681912507@gmail.com>
2023-08-31 23:01:27 -0400

    Fix(config): remove sb- prefix from packages name

--- packages/server/src/modules/user/users.module.ts
+++ packages/server/src/modules/user/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { UsersService } from 'services/users.service';
 import { UsersController } from '../../controllers/users.controller';
+import { UsersService } from 'services/users.service';
 import { UsersRepository } from 'repositories/users.repository';
 
 import { DataSourceProvider } from 'configs/dataSource.config';
