403133eb19f5d43dbb45279c1a27376e776d413d
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2024-08-21 23:02:09 -0400

    Feat(repo): initialize Java API package

---
+++ packages/java-api/src/main/java/com/m20a23x/games/GamesApplication.java
@@ -0,0 +1,13 @@
+package com.m20a23x.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesApplication.class, args);
+	}
+
+}
